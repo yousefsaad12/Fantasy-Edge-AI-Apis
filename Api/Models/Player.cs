91968f75c0d9e0374a2bf61b4627793493de56e9
Yousef Saad <saadrslan95@gmail.com>
2024-08-23 18:40:23 +0100

    Player Model added

---
+++ Api/Models/Player.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Models
+{
+    public class Player
+    {   
+        public int PlayerId { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string WebName { get; set; }
+        public int TeamId { get; set; } 
+        public int ElementTypeId { get; set; }
+        public int ?  SquadNumber { get; set; } 
+        public char Status { get; set; }
+        public string ?  News { get; set; }
+        public DateTime ?  NewsAdded { get; set; }
+        public int ? ChancePlayingNextRound { get; set; }
+        public int ? ChancePlayingThisRound { get; set; }
+
+    }
+}
