b2ac49cf78a4d27c4e37a3b4eaf2a341e56037d1
Yousef Saad <saadrslan95@gmail.com>
2024-08-23 19:14:09 +0100

    Play Statistics model added

---
+++ Api/Models/PlayerStatistics.cs
@@ -0,0 +1,28 @@
+
+
+namespace Api.Models
+{
+    public class PlayerStatistics
+    {
+      public int PlayerStatisticsId { get; set; }  
+      public decimal Influence  { get; set; }  
+      public decimal Creativity  { get; set; }  
+      public decimal Threat { get; set; }  
+      public decimal IctIndex  { get; set; }  
+      public decimal ExpectedGoals { get; set; }  
+      public decimal ExpectedAssists { get; set; }  
+      public decimal ExpectedGoalInvolvements { get; set; }  
+      public decimal ExpectedGoalsConceded { get; set; }  
+      public decimal ExpectedGoalsPer90  { get; set; }  
+      public decimal ExpectedAssistsPer90  { get; set; }  
+      public decimal ExpectedGoalInvolvementsPer90 { get; set; }  
+      public decimal ExpectedGoalsConcededPer90 { get; set; }  
+      public decimal GoalsConcededPer90  { get; set; }  
+      public decimal StartsPer90  { get; set; }  
+      public decimal CleanSheetsPer90  { get; set; }
+      
+      public int PlayerId { get; set; }
+      public Player player { get; set; }
+
+    }
+}
--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77b4deeb9611b627f1e69cfdb94598bbd885f6b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b284886e186168c6e87c5022183cd0627ccc26c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
