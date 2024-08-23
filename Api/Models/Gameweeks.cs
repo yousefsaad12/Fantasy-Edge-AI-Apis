de03b06c76a3180846234ba6976785a236d2376c
Yousef Saad <saadrslan95@gmail.com>
2024-08-23 18:45:55 +0100

    GameWeek Model added

---
+++ Api/Models/Gameweeks.cs
@@ -0,0 +1,9 @@
+namespace Api.Models
+{
+    public class Gameweeks
+    {
+        public int GameweekID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
