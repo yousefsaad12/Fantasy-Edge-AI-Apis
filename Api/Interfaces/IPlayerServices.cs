4b14db7ee4996ccc5ccd89d26d91489b7fad8177
yousefsaad12 <saadrslan95@gmail.com>
2024-09-14 22:24:09 +0100

    change some services

--- Api/Interfaces/IPlayerServices.cs
+++ Api/Interfaces/IPlayerServices.cs
@@ -9,6 +9,6 @@ namespace Api.Interfaces
         public Task<IEnumerable<Player>> ? GetPlayersAsync();
         public Task<bool> CreatePlayer(Player player);
         public  Task InsertPlayersAndRelatedEntitiesAsync(IEnumerable<PlayerJsonForm> playerJsonForms);
-        public  Task<bool> UpdatePlayer(Player existingPlayer, Player newPlayerData, PlayerPerformance pp, PlayerStatistics ps, PlayerTransfer pt, PlayerValue pv);
+        public  Task<bool> UpdatePlayer(Player existingPlayer, Player newPlayerData, PlayerPerformance pp, PlayerStatistics ps);
     }
 }
--- Api/Services/PlayerServices.cs
+++ Api/Services/PlayerServices.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore.Query.Internal;
+
 
 namespace Api.Services
 {
@@ -65,9 +65,7 @@ namespace Api.Services
 
                 Player ? player = await _unitOfWork.Players.GetByName(FirstName, SecondName,
                                                     p => p.PlayerPerformances,
-                                                    p => p.PlayerStatistics,
-                                                    p => p.PlayerTransfers,
-                                                    p => p.PlayerValues);
+                                                    p => p.PlayerStatistics);
 
 
                 return player != null ? player : null;
@@ -82,133 +80,111 @@ namespace Api.Services
         public async Task<IEnumerable<Player>> ? GetPlayersAsync()
         {
             return await _unitOfWork.Players.GetAll(p => p.PlayerPerformances, 
-                                                    p => p.PlayerStatistics,
-                                                    p => p.PlayerValues,
-                                                    p => p.PlayerTransfers);
+                                                    p => p.PlayerStatistics);
       
         }
 
-        public Task InsertPlayersAndRelatedEntitiesAsync(IEnumerable<PlayerJsonForm> playerJsonForms)
+        public async Task InsertPlayersAndRelatedEntitiesAsync(IEnumerable<PlayerJsonForm> playerJsonForms)
         {
-            throw new NotImplementedException();
+            List<Player> players = playerJsonForms.Select(p => p.MapToPlayer()).ToList();
+            List<PlayerStatAndPerJson>Performance = playerJsonForms.Select(p => p.playerStatAndPerJson).ToList();
+            List<PlayerPerformance> playerPerformances = Performance.Select(p => p.MapToPlayerPerformance()).ToList();
+            List<PlayerStatistics> playerStatistics =Performance.Select(p => p.MapPlayerStatistics()).ToList();
+
+            const int batchSize = 100;
+            var playerList = players.ToList();
+          try{
+
+             // await _genericRepo.BeginTransactionAsync();
+
+                for (int i = 0; i < playerList.Count; i += batchSize)
+                {
+                    var batch = playerList.Skip(i).Take(batchSize).ToList();
+
+                    foreach (var player in batch)
+                    {   
+
+                        Player existingPlayer = await GetPlayerbyName(player.FirstName, player.SecondName);
+
+                        var pp = playerPerformances.FirstOrDefault(p => p.PlayerId == player.PlayerId);
+                        var ps = playerStatistics.FirstOrDefault(p => p.PlayerId == player.PlayerId);
+
+
+
+                        if(existingPlayer is null)
+                        {
+
+                            player.PlayerPerformances.Add(pp);
+                            player.PlayerStatistics.Add(ps);
+                            // Insert the new player into the database
+                            await CreatePlayer(player);
+                        }
+                        else await UpdatePlayer(existingPlayer, player, pp, ps);
+
+
+                    }
+                }
+                // Save the changes for the current batch
+                //await _genericRepo.CommitTransactionAsync();
+                _logger.LogInformation("All players inserted/updated successfully.");
+
+            }
+
+            catch (Exception ex)
+            {   
+                //await _genericRepo.RollbackTransactionAsync();
+                _logger.LogError(ex, "An error occurred while inserting/updating players.");
+                throw;
+            }
+
         }
 
-        public Task<bool> UpdatePlayer(Player existingPlayer, Player newPlayerData, PlayerPerformance pp, PlayerStatistics ps, PlayerTransfer pt, PlayerValue pv)
-        {
-            throw new NotImplementedException();
+
+            public async Task<bool> UpdatePlayer(Player existingPlayer, Player newPlayerData, PlayerPerformance pp, PlayerStatistics ps)
+            {
+                try
+                {
+                    // Ensure the new player data has valid names and status
+                    if (string.IsNullOrWhiteSpace(newPlayerData.FirstName) || string.IsNullOrWhiteSpace(newPlayerData.SecondName) || string.IsNullOrWhiteSpace(newPlayerData.Status))
+                    {
+                        _logger.LogWarning("Attempted to update a player with invalid names: {FirstName} {SecondName} {Status}", newPlayerData.FirstName, newPlayerData.SecondName, newPlayerData.Status);
+                        throw new ArgumentException("Player names cannot be null or empty.");
+                    }
+
+                    // Check if the existing player is valid
+                    if (existingPlayer is null)
+                    {
+                        _logger.LogWarning("Existing player is null, update cannot proceed.");
+                        return false;
+                    }
+
+                    // Use PlayerHelper to update the existing player's properties with the new data
+                    await PlayerUpdateHelper.UpdateBasicPlayerProperties(existingPlayer, newPlayerData);
+
+                    existingPlayer.PlayerPerformances.Add(pp);
+                    existingPlayer.PlayerStatistics.Add(ps);
+                    // Save the changes to the repository
+                    bool isSuccess =  await _unitOfWork.Players.UpdateOne(existingPlayer);
+
+                    if (isSuccess)
+                    {
+                        _logger.LogInformation("Player successfully updated: {FirstName} {SecondName}", newPlayerData.FirstName, newPlayerData.SecondName);
+                    }
+                    else
+                    {
+                        _logger.LogWarning("Failed to update player: {FirstName} {SecondName}", newPlayerData.FirstName, newPlayerData.SecondName);
+                    }
+
+                    return isSuccess;
+                }
+
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "An error occurred while updating player: {FirstName} {SecondName}", newPlayerData.FirstName, newPlayerData.SecondName);
+                    throw;
+                }
+            }
+
         }
-
-        // public async Task InsertPlayersAndRelatedEntitiesAsync(IEnumerable<PlayerJsonForm> playerJsonForms)
-        // {
-        //     List<Player> players = playerJsonForms.Select(p => p.MapToPlayer()).ToList();
-        //     IEnumerable<PlayerPerformance> playerPerformances = playerJsonForms.Select(p => p.MapToPlayerPerformance()).ToImmutableList();
-        //     IEnumerable<PlayerStatistics> playerStatistics = playerJsonForms.Select(p => p.MapPlayerStatistics()).ToImmutableList();
-        //     IEnumerable<PlayerTransfer> playerTransfer = playerJsonForms.Select(p => p.MapPlayerTransfer()).ToImmutableList();
-        //     IEnumerable<PlayerValue> playerValue = playerJsonForms.Select(p => p.MapPlayerValue()).ToImmutableList();
-
-        //     const int batchSize = 100;
-        //     var playerList = players.ToList();
-        //   try{
-
-        //      // await _genericRepo.BeginTransactionAsync();
-
-        //         for (int i = 0; i < playerList.Count; i += batchSize)
-        //         {
-        //             var batch = playerList.Skip(i).Take(batchSize).ToList();
-
-        //             foreach (var player in batch)
-        //             {   
-
-        //                 Player existingPlayer = await GetPlayerbyName(player.FirstName, player.SecondName);
-
-        //                 var pp = playerPerformances.FirstOrDefault(p => p.PlayerId == player.PlayerId);
-        //                 var ps = playerStatistics.FirstOrDefault(p => p.PlayerId == player.PlayerId);
-        //                 var pt = playerTransfer.FirstOrDefault(p => p.PlayerId == player.PlayerId);
-        //                 var pv = playerValue.FirstOrDefault(p => p.PlayerId == player.PlayerId);
-
-
-
-        //                 if(existingPlayer is null)
-        //                 {
-
-        //                     player.PlayerPerformances.Add(pp);
-        //                     player.PlayerStatistics.Add(ps);
-        //                     player.PlayerTransfers.Add(pt);
-        //                     player.PlayerValues.Add(pv);
-
-        //                     // Insert the new player into the database
-        //                     await CreatePlayer(player);
-        //                 }
-        //                 else await UpdatePlayer(existingPlayer, player, pp, ps, pt, pv);
-
-
-        //             }
-        //         }
-        //         // Save the changes for the current batch
-        //         //await _genericRepo.CommitTransactionAsync();
-        //         _logger.LogInformation("All players inserted/updated successfully.");
-
-        //     }
-
-        //     catch (Exception ex)
-        //     {   
-        //         //await _genericRepo.RollbackTransactionAsync();
-        //         _logger.LogError(ex, "An error occurred while inserting/updating players.");
-        //         throw;
-        //     }
-
-        // }
-
-
-        //     public async Task<bool> UpdatePlayer(Player existingPlayer, Player newPlayerData, PlayerPerformance pp, PlayerStatistics ps, PlayerTransfer pt, PlayerValue pv)
-        //     {
-        //         try
-        //         {
-        //             // Ensure the new player data has valid names and status
-        //             if (string.IsNullOrWhiteSpace(newPlayerData.FirstName) || string.IsNullOrWhiteSpace(newPlayerData.SecondName) || string.IsNullOrWhiteSpace(newPlayerData.Status))
-        //             {
-        //                 _logger.LogWarning("Attempted to update a player with invalid names: {FirstName} {SecondName} {Status}", newPlayerData.FirstName, newPlayerData.SecondName, newPlayerData.Status);
-        //                 throw new ArgumentException("Player names cannot be null or empty.");
-        //             }
-
-        //             // Check if the existing player is valid
-        //             if (existingPlayer is null)
-        //             {
-        //                 _logger.LogWarning("Existing player is null, update cannot proceed.");
-        //                 return false;
-        //             }
-
-        //             // Use PlayerHelper to update the existing player's properties with the new data
-        //             await PlayerUpdateHelper.UpdateBasicPlayerProperties(existingPlayer, newPlayerData);
-
-        //             existingPlayer.PlayerPerformances.Add(pp);
-        //             existingPlayer.PlayerStatistics.Add(ps);
-        //             existingPlayer.PlayerTransfers.Add(pt);
-        //             existingPlayer.PlayerValues.Add(pv);
-
-        //             // Save the changes to the repository
-        //             bool isSuccess =  await _unitOfWork.Players.UpdateOne(existingPlayer);
-
-        //             if (isSuccess)
-        //             {
-        //                 _logger.LogInformation("Player successfully updated: {FirstName} {SecondName}", newPlayerData.FirstName, newPlayerData.SecondName);
-        //             }
-        //             else
-        //             {
-        //                 _logger.LogWarning("Failed to update player: {FirstName} {SecondName}", newPlayerData.FirstName, newPlayerData.SecondName);
-        //             }
-
-        //             return isSuccess;
-        //         }
-
-        //         catch (Exception ex)
-        //         {
-        //             _logger.LogError(ex, "An error occurred while updating player: {FirstName} {SecondName}", newPlayerData.FirstName, newPlayerData.SecondName);
-        //             throw;
-        //         }
-        //     }
-
-        // }
     }
 
-}
